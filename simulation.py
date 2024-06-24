2235e80644daca4515da20c3cb3feeb8c8a59559
Diana Hannah Gonia <fielyan2@gmail.com>
2024-06-24 00:14:06 -0700

    Add files via upload

---
+++ simulation.py
@@ -0,0 +1,5 @@
+from random import choice
+
+choices = ["ZEVEL", "AIRON", "JED", "SHAMON", "ACE", "ALDRAIN", "JHOREN", "ALVIN"]
+
+print(choice(choices))
---
+++ simulation2.py
@@ -0,0 +1,53 @@
+#Discussion of list
+'''t = (1, 2, 3, 4, 5, 0) #tuple is immutable / unchangable
+l = [1, 2, 3, 4, 5, 0] #List is mutable / changable
+#MUTABLE - Can be change
+#IMMUTABLE - Cannot be change 
+print(t[1:4])
+print(l[2:3])
+
+l[5] = 6
+t[5] = 6
+print(l)
+print(t)'''
+
+'''l = [5, 4, 7, 1, 2]
+#l.append(3)
+#print(l)
+l.insert(1, 6)
+print(l)'''
+
+'''l = [5, 4, 7, 1, 2]
+#l[1] = 3
+l.replace()
+print(l)'''
+
+'''l = [5, 4, 7, 1, 2]
+l.remove(7)
+l.pop(-1)
+print(l)'''
+
+#stack = first in last out
+#queue = first in first out
+
+'''l = [5, 4, 7, 1, 2]
+l.insert(1, 3)
+l.append(6)
+print(l)'''
+
+'''l = [5, 4, 7, 1, 2]
+l.remove(1)
+l.pop(-3)
+print(l)'''
+
+'''l = [5, 4, 7, 1, 2]
+l.clear()
+print(l)'''
+
+'''l = [5, 4, 7, 1, 2]
+l.sort(reverse = True)
+print(l)'''
+
+l = [5, 4, 7, 1, 2]
+l.reverse()
+print(l)
