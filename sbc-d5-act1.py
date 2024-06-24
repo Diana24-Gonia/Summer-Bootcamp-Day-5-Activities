e379423f724d01c209280a10eeb6708edef8e26b
Diana Hannah Gonia <fielyan2@gmail.com>
2024-06-24 00:21:51 -0700

    sbc-d5-act1.py

--- sbc-d5-act1.py
+++ sbc-d5-act1.py
@@ -4,22 +4,20 @@ corrupt.append(int(input("Enter number 1: ")))
 corrupt.append(int(input("Enter number 2: ")))
 corrupt.append(int(input("Enter number 3: ")))
 corrupt.append(int(input("Enter number 4: ")))
-corrupt.append(int(input("Enter number 3: ")))
+corrupt.append(int(input("Enter number 5: ")))
 
 print("Display User Numbers:", corrupt)
 
 act =""
-
+act = input("Enter 'naa' for queue operation, 'wala' for stack operation: ").strip().lower()
 if act == "naa":
     corrupt.pop(0)
     print("Result sa queue boss: ", corrupt)
-
 elif act == "wala": 
     corrupt.pop()
     print("Result sa stack boss: ", corrupt)
     corrupt.append(int(input("Enter add number: ")))
     corrupt.append(int(input("Enter add number: ")))
     print("insert add number result: ", corrupt)
-
 else:
     print("Invalid input. kung dili Naa or Wala.")
